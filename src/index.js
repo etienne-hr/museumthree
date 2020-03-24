80d5795598e64142cdd7173ad094f6f26b28ebc4
zoevincent <zoe.vincent@hetic.net>
2020-03-24 11:43:26 +0100

    three.js and files structures added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,109 @@
 import './style/main.styl'
+import * as THREE from 'three'
+// import River from './javascript/River.js'
+import Louvre from './javascript/Louvre.js'
+// import Orsay from './javascript/Orsay.js'
+// import Pompidou from './javascript/Pompidou.js'
+// import Palais from './javascript/Palais.js'
 
-console.log('Hello Webpack')
+/**
+ * Sizes
+ */
+const sizes = {}
+sizes.width = window.innerWidth
+sizes.height = window.innerHeight
+
+/**
+ * Cursor
+ */
+const cursor = {}
+cursor.x = 0
+cursor.y = 0
+
+window.addEventListener('mousemove', (_event) => {
+    cursor.x = _event.clientX / sizes.width - 0.5
+    cursor.y = _event.clientY / sizes.height - 0.5
+})
+
+/**
+ * Images
+ */
+// ici instancier les photos de chaque musée
+
+/**
+ * Scene
+ */
+const scene = new THREE.Scene()
+
+/**
+ * Lights
+ */
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.2)
+scene.add(ambientLight)
+
+
+
+/**
+ * Objects
+ */
+
+// River
+
+// Louvre
+const louvre = new Louvre()
+scene.add(louvre.group)
+
+// Orsay
+
+// Pompidou
+
+// Palais
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 20)
+camera.position.z = 3
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(window.devicePixelRatio)
+document.body.appendChild(renderer.domElement)
+
+/**
+ * Resize
+ */
+window.addEventListener('resize', () => {
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width, sizes.height)
+})
+
+/**
+ * Zoom
+ */
+// mettre ici le zoom au clic sur un musée
+
+/**
+ * Loop
+ */
+const loop = () => {
+    window.requestAnimationFrame(loop)
+
+    //camera
+    camera.lookAt(scene.position)
+
+    // Render
+    renderer.render(scene, camera)
+}
+
+loop()
---
+++ src/javascript/Louvre.js
@@ -0,0 +1,23 @@
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+
+export default class Louvre {
+    constructor() {
+        this.group = new THREE.Group()
+
+        const dracoLoader = new DRACOLoader()
+        dracoLoader.setDecoderPath('/draco/')
+
+        const gltfLoader = new GLTFLoader()
+        gltfLoader.setDRACOLoader(dracoLoader)
+
+        gltfLoader.load(
+            '/models/Louvre.gltf',
+            (_gltf) => {
+                this.louvre = _gltf.scene.children[0]
+                this.group.add(this.louvre)
+            }
+        )
+    }
+}
